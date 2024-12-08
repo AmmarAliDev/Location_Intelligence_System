a11d4d49880008b51da078264062ebc11a916930
Syed Ammar Ali <ammar.ali.skipq@gmail.com>
2024-12-08 22:12:01 +0500

    - changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
 const express = require('express')
 const cors = require('cors')
 const vehicleRoutes = require('./routes/vehicleRoutes')
-const dbConnect = require('./config/db') // Assuming you have a dbConnect function
+const dbConnect = require('./config/db')
 
-// Connect to MongoDB
 dbConnect()
 
 const app = express()
 
-// Request logging middleware
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`)
   next()
 })
 
-// Middleware to parse incoming JSON requests
 app.use(express.json())
 
-// Use CORS middleware
-app.use(cors())
+// app.use(cors())
+app.use(
+  cors({
+    origin: 'https://location-intelligence-system-frontend.vercel.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+)
 
 // Routes
 app.use('/api/vehicles', vehicleRoutes)
@@ -29,7 +32,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Server Error', error: err.message })
 })
 
-// Start server
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
